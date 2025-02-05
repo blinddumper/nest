56c4c4f3bc9fac38545a1c575b1487673d2312f0
blinddumper <blinddumper@yahoo.com>
2025-02-05 12:52:22 +0000

    Revert styles - enhancing logs

--- scripts/extract-language.js
+++ scripts/extract-language.js
@@ -1,9 +1,9 @@
 // 本文件用来提取组件中的中文文案，并生成 hash 格式的语言包，简化文案提取工作
-var fs = require('fs')
-var path = require('path')
-var crypto = require('crypto')
-var fse = require('fs-extra')
-var shelljs = require('shelljs')
+const fs = require('fs')
+const path = require('path')
+const crypto = require('crypto')
+const fse = require('fs-extra')
+const shelljs = require('shelljs')
 
 // utils
 function unique(arr) {
@@ -11,31 +11,31 @@ function unique(arr) {
 }
 
 function hash(str) {
-  var hash = crypto.createHash('sha256')
+  const hash = crypto.createHash('sha256')
   hash.update(str)
   return hash.digest('hex').slice(0, 8)
 }
 
 // 输入组件名称，例如 Input 组件， node scripts/extract-language.js Input
 // 组件的 demo.tsx 中 demo 组件名字应该为 组件名称 + Demo，例如：InputDemo，CheckboxDemo 等
-var pkg = process.argv[2]
-var locals = ['zh-CN', 'zh-TW', 'en-US']
+const pkg = process.argv[2]
+const locals = ['zh-CN', 'zh-TW', 'en-US']
 
-var demoFile = path.join(
+const demoFile = path.join(
   process.cwd(),
   `src/packages/${pkg.toLocaleLowerCase()}/demo.tsx`
 )
 let demoContent = fs.readFileSync(demoFile).toString()
 
-var matchingChinese =
+const matchingChinese =
   /([0-9a-z]*[\u4e00-\u9fa5]+[0-9]*[\u4e00-\u9fa5()（）0-9a-z\/]*)/gi
-var unrepeatedChinese = unique(demoContent.match(matchingChinese))
+const unrepeatedChinese = unique(demoContent.match(matchingChinese))
 
-var localTable = {}
-var interfaceType = []
+const localTable = {}
+const interfaceType = []
 
 unrepeatedChinese.forEach((item) => {
-  var k = hash(item)
+  const k = hash(item)
   localTable[k] = item
   interfaceType.push(`"${k}": string`)
   // 要处理重复
@@ -51,23 +51,23 @@ unrepeatedChinese.forEach((item) => {
 })
 
 // interface
-var interfaceTemplate = `interface T {
+const interfaceTemplate = `interface T {
   ${interfaceType.join('\n  ')}
 }`
 
-var translate = {}
+const translate = {}
 locals.forEach((item) => (translate[item] = localTable))
-var translateHookTemplate = `
-var [translated] = useTranslate<T>(${JSON.stringify(translate, ' ', 2)})
+const translateHookTemplate = `
+const [translated] = useTranslate<T>(${JSON.stringify(translate, ' ', 2)})
 `
 
 demoContent =
   `import { useTranslate } from '../../sites/assets/locale'\n` +
   demoContent.replace(
-    `var ${pkg}Demo = () => {`,
+    `const ${pkg}Demo = () => {`,
     `
   ${interfaceTemplate}
-  var ${pkg}Demo = () => {
+  const ${pkg}Demo = () => {
     ${translateHookTemplate}
   `
   )
