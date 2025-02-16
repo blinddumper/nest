034bf1284dbdb34095d2af6f1457b797b38ec23a
blinddumper <blinddumper@yahoo.com>
2025-02-16 11:43:55 +0000

    Remove database - updating docs

--- scripts/generate-changelog.js
+++ scripts/generate-changelog.js
@@ -1,17 +1,17 @@
-var fs = require('fs-extra');
-var path = require('path');
-var changelog = fs.readFileSync(path.join(__dirname, '../CHANGELOG.md'), 'utf8');
+const fs = require('fs-extra');
+const path = require('path');
+const changelog = fs.readFileSync(path.join(__dirname, '../CHANGELOG.md'), 'utf8');
 
-var a = changelog.split('# v')
+const a = changelog.split('# v')
 console.log(a[1])
-var getLatestRelease = (cl) => {
-  var a = changelog.split('# v')
+const getLatestRelease = (cl) => {
+  const a = changelog.split('# v')
   if(a.length === 0) return ''
   return `# v${a[1]}`
 
-  // var tag1 = cl.indexOf('#')
+  // const tag1 = cl.indexOf('#')
   // if (tag1 > -1) {
-  //   var tag2 = cl.split('').slice(tag1 + 1, cl.length - 1).join('').indexOf('#')
+  //   const tag2 = cl.split('').slice(tag1 + 1, cl.length - 1).join('').indexOf('#')
   //   if (tag2 > -1) {
   //     return cl.substring(tag1, tag2)
   //   }
