816665cafceed1ef5e1649064fd46252b251fa25
blinddumper <blinddumper@yahoo.com>
2025-03-12 16:44:00 +0000

    Optimize dependencies - updating docs

--- scripts/generate-themes.js
+++ scripts/generate-themes.js
@@ -1,15 +1,15 @@
-var config = require('../src/config.json')
-var path = require('path')
-var fs = require('fs-extra')
-var glob = require('glob')
+const config = require('../src/config.json')
+const path = require('path')
+const fs = require('fs-extra')
+const glob = require('glob')
 
 let fileStr = `@import '../theme-default.scss';\n@import '../variables.scss';\n`
-var projectID = process.env.VITE_APP_PROJECT_ID
+const projectID = process.env.VITE_APP_PROJECT_ID
 if (projectID) {
   fileStr = `@import '../theme-${projectID}.scss';\n@import '../variables-${projectID}.scss';\n`
 }
 let tasks = []
-var componentsScss = glob.sync('./src/packages/**/*.scss', { dotRelative: true })
+const componentsScss = glob.sync('./src/packages/**/*.scss', { dotRelative: true })
 componentsScss.map((cs) => {
   if (cs.indexOf('demo.scss') > -1) return
   tasks.push(
